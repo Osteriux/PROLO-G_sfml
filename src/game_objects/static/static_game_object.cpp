ed8f79fc815181479aaac08e9c4a19229e4cd3a9
osteriux <pixave73@gmail.com>
2025-11-18 14:31:20 +0100

    fix critical

--- src/game_objects/static/static_game_object.cpp
+++ src/game_objects/static/static_game_object.cpp
@@ -1,6 +1,6 @@
 #include "static_game_object.h"
 
-StaticGameObject::StaticGameObject(std::unique_ptr<MultiTexture> animation, GameManager* , int x, int y)
+StaticGameObject::StaticGameObject(std::unique_ptr<MultiTexture> animation, GameManager *gameManager, int x, int y)
     : GameObject(std::move(animation), gameManager, x, y)
 {
 }
